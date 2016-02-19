e9c19f04d2e7b429cf21682642f72054b13e9e3a
Nik <nnonexistent@gmail.com>
2016-02-19 19:36:01 +0300

    Add cmd arguments parsing, misc improvements

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 Samfellu
 ========
-Visualize russian text in curvy line according to morphology. 
+Visualize russian text in curvy line according to morphology.
 
 
 TODO
 ====
+ * Process within memory if fits
  * Process text chapters
  * Ability to select color scheme
  * Normalization over global/chunk/chapter scope
- * Image fit in specified size
- * Command line arguments parsing
  * Make app reusable by external modules, publish maybe
  * Autopositioning for legend
+ * Autodetect encoding
+ * Print title on image
--- samfellu.py
+++ samfellu.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import re
+import argparse
+import exceptions
 import os
 import sys
 import codecs
@@ -11,10 +13,6 @@ import math
 import array
 from tempfile import TemporaryFile
 from collections import Counter
-
-
-INPUT_FILENAME = '8.txt'
-OUTPUT_FILENAME = 'out.png'
 
 
 class SamfelluError(Exception):
@@ -30,57 +28,54 @@ def rotate_vector(x, y, angle_deg):
 
 
 class Samfellu(object):
-    def __init__(self, text_input,
-            input_type='filename',
-            text_encoding='utf-8',
-            text_chunk_size=4096,
-            points_chunk_size=4096,
-            image_size=(640, 640),
-            image_line_width=1,
-            image_padding=.05,
-            image_draw_legend=True,
-            max_word_size=50):
+    text_encoding = 'utf-8'
+    text_chunk_size = 4096
+    max_word_size = 50
+    points_chunk_size = 4096
+    image_line_width = 1
+    image_padding = .05
+    image_draw_legend = True
+    directions = (
+        (u'Существительные', ('NOUN', )),
+        (u'Глаголы и деепричастия', ('VERB', 'INFN', 'GRND')),
+        (u'Прилагательные и причастия', ('ADJF', 'ADJS', 'PRTF', 'PRTS',)),
+        (u'Наречия', ('ADVB', 'COMP')),
+        (u'Союзы, предлоги и частицы', ('PREP', 'CONJ', 'PRCL')),
+        (u'Местоимения', ('NPRO', ))
+    )
+    colors = ((1, 0, 0), (0, 0, 1))
+
+    def __init__(self, text_input, input_type='filename', image_size=(640, 640), **kwargs):
+        # set options
         self.text_input = text_input
         self.input_type = input_type
-        self.text_encoding = text_encoding
-        self.text_chunk_size = text_chunk_size
-        self.points_chunk_size = points_chunk_size
         self.image_size = image_size
-        self.image_line_width = image_line_width
-        self.image_padding = image_padding
-        self.image_draw_legend = image_draw_legend
-        self.max_word_size = max_word_size
-        # directions = (
-        #     (u'существительные', ('noun', )),
-        #     (u'глаголы и деепричастия', ('verb', 'infn', 'grnd')),
-        #     (u'прилагательные и причастия', ('adjf', 'adjs', 'prtf', 'prts', 'advb', 'comp')),
-        #     (u'союзы, предлоги и пр.', ('pred', 'prep', 'conj', 'prcl', 'intj')),
-        #     (u'местоимения', ('npro', ))
-        # )
-        self.directions = (
-            (u'Существительные', ('NOUN', )),
-            (u'Глаголы и деепричастия', ('VERB', 'INFN', 'GRND')),
-            (u'Прилагательные и причастия', ('ADJF', 'ADJS', 'PRTF', 'PRTS',)),
-            (u'Наречия', ('ADVB', 'COMP')),
-            (u'Союзы, предлоги и частицы', ('PREP', 'CONJ', 'PRCL')),
-            (u'Местоимения', ('NPRO', ))
-        )
+        for k, v in kwargs.iteritems():
+            # simple options check
+            if k not in dir(self):
+                raise SamfelluError(u'Wrong option "%s"' % k)
+            setattr(self, k, v)
+
+        # initializing
         self.legend_pos = 0.1 * self.image_size[0], 0.1 * self.image_size[1]
         self.counter = Counter()  # directions counter
-        self.bbox = (0, 0, 0, 0)
+        self.bbox = (0, 0, 1, 1)
         self.total_words = 0
         self.tf_dir = None
         self.tf_points = None
+        self._morph = None
+        self._cairo_ctx = None
+        self._surface = None
 
     @property
     def morph(self):
-        if not hasattr(self, '_morph'):
+        if self._morph is None:
             self._morph = pymorphy2.MorphAnalyzer()
         return self._morph
 
     @property
     def cairo_ctx(self):
-        if not hasattr(self, '_cairo_ctx'):
+        if self._cairo_ctx is None:
             self._surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, *self.image_size)
             self._cairo_ctx = cairo.Context(self._surface)
             self._cairo_ctx.set_source_rgb(1, 1, 1)
@@ -101,7 +96,7 @@ class Samfellu(object):
 
     def get_color(self, i):
         t = float(i) / (self.total_words or 1)
-        start, end = (1, 0, 0), (0, 0 ,1)
+        start, end = self.colors[:2]
         return [start[i] + t*(end[i]-start[i]) for i in range(3)]
 
     def iter_text(self):
@@ -112,6 +107,8 @@ class Samfellu(object):
                 raise SamfelluError(u'Unable to open file "%s"' % self.text_input)
             except ValueError, e:
                 raise SamfelluError(u'Wrong encoding "%s" for file "%s"' % (self.text_encoding, self.text_input))
+            except exceptions.LookupError, e:
+                raise SamfelluError(u'Wrong encoding "%s"' % self.text_encoding)
 
             while True:
                 chunk = f.read(self.text_chunk_size)
@@ -120,6 +117,23 @@ class Samfellu(object):
                 yield chunk
 
             f.close()
+
+        elif self.input_type == 'stream':
+            try:
+                info = codecs.lookup(self.text_encoding)
+            except exceptions.LookupError, e:
+                raise SamfelluError(u'Wrong encoding "%s"' % self.text_encoding)
+            sr = info.streamreader(self.text_input)
+            sr.encoding = self.text_encoding
+
+            while True:
+                try:
+                    chunk = sr.read(self.text_chunk_size)
+                except ValueError, e:
+                    raise SamfelluError(u'Wrong encoding "%s" for stream' % self.text_encoding)
+                if not chunk:
+                    break
+                yield chunk
 
         elif self.input_type in ('str', 'string', 'unicode'):
             if isinstance(self.text_input, str):
@@ -230,7 +244,7 @@ class Samfellu(object):
             dx, dy = rotate_vector(-50, 0, 360 * i / len(self.directions))
             self.cairo_ctx.line_to(self.legend_pos[0] + dx, self.legend_pos[1] + dy)
             self.cairo_ctx.stroke()
-    
+
             self.cairo_ctx.move_to(self.legend_pos[0] + dx, self.legend_pos[1] + dy)
             self.cairo_ctx.set_source_rgb(0, 0, 0)
             self.cairo_ctx.show_text(u'%s (%s)' % (title, self.counter[i]))
@@ -240,7 +254,7 @@ class Samfellu(object):
         self.parse_words()
         self.construct_line()
         self.draw()
-        if self.draw_legend:
+        if self.image_draw_legend:
             self.draw_legend()
 
     def progress(self, words=None, line_points=None, drawn_points=None):
@@ -248,29 +262,73 @@ class Samfellu(object):
         pass
 
     def write_output(self, filename):
+        if self._surface is None:
+            raise SamfelluError(u'Drawing context is not ready')
         self._surface.write_to_png(filename)
 
 
 class ConsoleSamfellu(Samfellu):
     def progress(self, words=None, line_points=None, drawn_points=None):
+        def print_progress(text, width=79):
+            sys.stdout.write('    ' + text.ljust(width) + '\r')
+            sys.stdout.flush()
         if words:
-            sys.stdout.write(u'Words processed: %s\r' % words)
-            sys.stdout.flush()
+            print_progress(u'Words processed: %s' % words)
         elif line_points:
-            sys.stdout.write(u'Line construction: %s%%\r' % (100*line_points/self.total_words))
-            sys.stdout.flush()
+            print_progress(u'Line construction: %s%%' % (100*line_points/self.total_words))
         elif drawn_points:
-            sys.stdout.write(u'Drawing: %s%%\r' % (100*drawn_points/self.total_words))
-            sys.stdout.flush()
+            print_progress(u'Drawing: %s%%\r' % (100*drawn_points/self.total_words))
+
+# directions = (
+#     (u'существительные', ('noun', )),
+#     (u'глаголы и деепричастия', ('verb', 'infn', 'grnd')),
+#     (u'прилагательные и причастия', ('adjf', 'adjs', 'prtf', 'prts', 'advb', 'comp')),
+#     (u'союзы, предлоги и пр.', ('pred', 'prep', 'conj', 'prcl', 'intj')),
+#     (u'местоимения', ('npro', ))
+# )
+
+
+def print_error(error):
+    sys.stderr.write(u'\033[91mError: %s\033[0m\n' % error)
 
 
 def main():
-    smf = ConsoleSamfellu(INPUT_FILENAME)
-    smf.process()
-    print '\n'
-    for i, (title, poss) in enumerate(smf.directions):
-        print u'%s: %s' % (title, smf.counter[i])
-    smf.write_output(OUTPUT_FILENAME)
+    parser = argparse.ArgumentParser(description='Visualize russian text in curvy line according to morphology.')
+    parser.add_argument('input', help=u'Input text file')
+    parser.add_argument('output', help=u'Output image file')
+    parser.add_argument('-c', '--encoding', help=u'Input text encoding', default='utf-8')
+    parser.add_argument('-s', '--size', help=u'Image size', default='640x640')
+    parser.add_argument('-l', '--legend', action='store_true', help=u'Draw a legend')
+
+    args = parser.parse_args()
+
+    try:
+        w, h = map(int, args.size.split('x', 2))
+        size = (w, h)
+    except ValueError, e:
+        print_error(u'Wrong size format "%s"' % args.size)
+        return
+
+    kwargs = {
+        'text_encoding': args.encoding,
+        'text_input': args.input,
+        'image_size': size,
+        'image_draw_legend': args.legend,
+    }
+    if args.input == '-':
+        kwargs.update({
+            'text_input': sys.stdin,
+            'input_type': 'stream',
+        })
+    try:
+        smf = ConsoleSamfellu(**kwargs)
+        smf.process()
+        print '\n'
+        for i, (title, poss) in enumerate(smf.directions):
+            print u'%s: %s' % (title, smf.counter[i])
+        smf.write_output(args.output)
+    except SamfelluError, e:
+        print_error(e)
 
 
 if __name__ == "__main__":
